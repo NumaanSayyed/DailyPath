bd15642214bdaa80b830b91e03effe74335efa00
Danish <156919648+danishshaikh05@users.noreply.github.com>
2024-12-26 14:56:18 +0530

    Changes made in UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,76 +1,73 @@
 import React from 'react';
+import routine from '../images/fitness_routine.png'
+import { NavLink } from 'react-router-dom';
 
 function Footer() {
   return (
     <>
-
       <footer className="relative pt-8 pb-6">
-        <div className="container mx-auto px-4">
-          <div className="flex flex-wrap text-left lg:text-left">
-            <div className="w-full lg:w-6/12 px-4">
-              <h4 className="text-3xl fonat-semibold text-blueGray-700">Let's keep in touch!</h4>
-              <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
-                Find us on any of these platforms, we respond 1-2 business days.
-              </h5>
+
+        {/* footer upper section  */}
+          <div className="container px-5 py-24 mx-auto flex justify-center items-center md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+            <div className="w-96 flex-shrink-0 ml-[65px] justify-center items-center md:mx-0 mx-auto text-center md:text-left">
+              <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+                <span className="ml-28 text-3xl font-bold">
+                    DailyPath
+                </span>
+              </a>
+              <p className="mt-2 ml-28 text-lg leading-8 text-gray-1000">Find us on any of these platforms, we respond 1-2 business days.
               <div className="mt-6 lg:mb-0 mb-6">
                 <button className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-                  <i className="fab fa-twitter" /></button><button className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-                  <i className="fab fa-facebook-square" /></button><button className="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-                  <i className="fab fa-dribbble" /></button><button className="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+                  <i className="fab fa-twitter" />
+                </button>
+                <button className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+                  <i className="fab fa-facebook-square" />
+                </button>
+                <button className="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+                  <i className="fab fa-dribbble" />
+                </button>
+                <button className="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
                   <i className="fab fa-github" />
                 </button>
               </div>
-            </div>
-            <div className="w-full lg:w-6/12 px-4">
-              <div className="flex flex-wrap items-top mb-6">
-                <div className="w-full lg:w-4/12 px-4 ml-auto">
-                  <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Useful Links</span>
-                  <ul className="list-unstyled">
-                    <li>
-                      <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="#">Home</a>
-                    </li>
-                    <li>
-                      <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="#">Blog</a>
-                    </li>
-                    <li>
+              </p>
+              </div>
 
-                    </li>
-                  </ul>
-                </div>
-                <div className="w-full lg:w-4/12 px-4">
-                  <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Other Resources</span>
-                  <ul className="list-unstyled">
-                    <li>
-                      <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="#">MIT License</a>
-                    </li>
-                    <li>
-                      <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="#">Terms &amp; Conditions</a>
-                    </li>
-                    <li>
-                      <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="#">Privacy Policy</a>
-                    </li>
-                    <li>
-                      <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="
-                #">Contact Us</a>
-                    </li>
-                  </ul>
-                </div>
+
+             {/* UseFull links seciton   */}
+            <div className="flex-grow flex justify-center items-center flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+              <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                <h2 className="title-font font-[500] font-yeseva text-xl  mb-3">Useful links</h2>
+                <nav className="list-none mb-10 leading-10 text-gray-1000 text-base font-[400] font-OpenSans">
+                  <li><NavLink to="/" className="">Home</NavLink></li>
+                  <li><NavLink to="milestones" className="">Milestones</NavLink></li>
+                  <li><NavLink to="milestones" className="">Contact us</NavLink></li>
+                  
+                </nav>
+              </div>
+
+              {/* other resources section */}
+              <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                <h2 className="title-font font-[500] text-xl   mb-3">Other Resources </h2>
+                <nav className="list-none mb-10 text-gray-1000 leading-10 text-base font-[400] ">
+                <li><NavLink to="/" className="">MIT License</NavLink></li>
+                <li><NavLink to="milestones" className="">Terms & conditions</NavLink></li>
+                <li><NavLink to="milestones" className="">Privacy policy</NavLink></li>
+                </nav>
               </div>
             </div>
           </div>
-          <hr className="my-6 border-blueGray-300" />
-          <div className="flex flex-wrap items-center md:justify-between justify-center">
-            <div className="w-full md:w-4/12 px-4 mx-auto text-center">
-              <div className="text-sm text-blueGray-500 font-semibold py-1">
-                Copyright © <span id="get-current-year">2024</span><a href="
-          #" className="text-blueGray-500 hover:text-gray-800" target="_blank"> Numaan
-                </a><a href="#" className="text-blueGray-500 hover:text-blueGray-800">Sayyed</a>.
-              </div>
+
+          {/* copywrite section  */}
+          <div className="mx-auto border-t-[1px] border-solid flex justify-center items-center w-[70%]"></div>
+
+          <div className= "p-6">
+            <div className="container mx-auto text-gray-1000 py-4 px-5 flex justify-center items-center flex-wrap flex-col sm:flex-row">
+              <p>Copyright  &copy; 2024 | All rights reserved</p>
             </div>
           </div>
-        </div>
+          
       </footer>
-
     </>
   );
 }
